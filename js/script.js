d810307798d3f939125382eee8859c5553b02777
Magda Lejwoda <le.magda@o2.pl>
2019-09-02 19:38:44 +0200

    Add generate tags function

--- js/script.js
+++ js/script.js
@@ -53,7 +53,8 @@ const titleClickHandler = function(event){
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){
 
@@ -106,28 +107,35 @@ function clearTitleList(){
 
 function generateTags(){
   /* find all articles */
-
+const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
-
+  for (let article of articles){
     /* find tags wrapper */
-
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
     /* make html variable with empty string */
-
+    let html = '';
     /* get tags from data-tags attribute */
-
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
     /* split tags into array */
-
+    const articleTagsArray = articleTags.split('');
+    console.log(articleTagsArray);
     /* START LOOP: for each tag */
-
+      for(let tag of articleTagsArray){
+        console.log(tag);
       /* generate HTML of the link */
-
+        const linkHTML = '<li><a href="#' + tag + '">' + tag + '</a></li>';
+        console.log(linkHTML);
       /* add generated code to html variable */
+      tagsWrapper.insertAdjacentHTML('beforeend', linkHTML);
 
     /* END LOOP: for each tag */
-
+      html = html + linkHTML;
+      }
     /* insert HTML of all the links into the tags wrapper */
-
+      tagsWrapper.innerHTML = html;
   /* END LOOP: for every article: */
+  }
 }
 
 generateTags();
