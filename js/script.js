ddd36db4deb4e53b9361119eb6bf0a7b5e573463
Magda Lejwoda <le.magda@o2.pl>
2019-09-12 23:09:52 +0200

    Generate list of authors of the right

--- js/script.js
+++ js/script.js
@@ -270,7 +270,7 @@ ________________________________________
  */
 
 function calculateAuthorsParams(authors){
-  console.log('calculate authors', calculateAuthorsParams);
+  //console.log('calculate authors', calculateAuthorsParams);
   const params =
   {
     max: 0,
@@ -302,19 +302,19 @@ function generateAuthors(){
   let allAuthors = {};
 
   /* find all authors */
-  const authors = document.querySelectorAll(optArticleSelector);
+  const articles = document.querySelectorAll(optArticleSelector);
   //console.log('Those are authors:', authors);
   /* START LOOP for each authors */
-  for(let author of authors){
-    const authorsWrapper = author.querySelector(optArticleAuthorSelector);
+  for(let article of articles){
+    const authorsWrapper = article.querySelector(optArticleAuthorSelector);
    // console.log("Here is author wrapper", authorWrapper);
       /* make html variable with empty string */
     let html = '';
       /* get authors from data-authors attribute */
-    const articleAuthor = author.getAttribute('data-author');
+    const articleAuthor = article.getAttribute('data-author');
      // console.log('get authors from attribute', articleAuthor);
       /* generate HTML of the link of author */
-    const authorHTML = '<a href="#' + articleAuthor + '">' + articleAuthor + '</a>';
+    const authorHTML = '<a href="#author-' + articleAuthor + '">' + articleAuthor + '</a>';
        //  console.log('link: ', authorHTML);
     //authorsWrapper.insertAdjacentHTML('beforeend', authorHTML)
 
@@ -333,7 +333,7 @@ function generateAuthors(){
       }
 
     /* [new] find list of authors in right column*/
-    const authorsList = document.querySelector('.authors')  ;
+    const authorList = document.querySelector('.authors');
     console.log(allAuthors);
 
     const authorsParams = calculateAuthorsParams(allAuthors);
@@ -348,9 +348,11 @@ function generateAuthors(){
       allAuthorsHTML += `<li><a class="${optCloudAuthorClassPrefix + calculateAuthorClass(allAuthors[author], authorsParams)}" href="${author}"><span>${author}</span></a></li>`;
     }
     /* [new] add html from allAuthorsHTML to authorList*/
-    authorsList.innerHTML = allAuthorsHTML;
+    authorList.innerHTML = allAuthorsHTML;
 }
+
 generateAuthors();
+
 addClickListenersToAuthors();
 
 function addClickListenersToAuthors () {
@@ -373,7 +375,7 @@ function authorClickHandler(event) {
   /* make a new const 'href' and read the attribute href of the clicked element */
   const href = clickedElement.getAttribute('href');
   /* make a new const 'author' and extract author from the 'href' constant */
-  const author = href.replace('#', '');
+  const author = href.replace('#author-', '');
   /* find all author links with class active */
   const activeAuthorLinks = document.querySelectorAll('.post-author a.active');
     /*START LOOP: for each active author Link*/
